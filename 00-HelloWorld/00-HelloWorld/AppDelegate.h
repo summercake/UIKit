fd7c2df8c8f164ca0e3169aa08653bc41ac30e13
jack <summercake@qq.com>
2017-11-26 05:10:40 -0600

    hello world

---
+++ 00-HelloWorld/00-HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  00-HelloWorld
+//
+//  Created by jack on 11/26/17.
+//  Copyright © 2017 OCNotes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 00-HelloWorld/00-HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  00-HelloWorld
+//
+//  Created by jack on 11/26/17.
+//  Copyright © 2017 OCNotes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
